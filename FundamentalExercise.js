6e2839fa9129db1d10c9ec65f69ec45dfed20e5d
Angelique <javathehut@users.noreply.github.com>
2017-03-22 18:09:06 -0400

    Completed Fundamental Exercises 1 to 6.

--- FundamentalExercise.js
+++ FundamentalExercise.js
@@ -1,12 +1,33 @@
+//Exercise 1
 var name = "Angelique";
 console.log("Hello" + " " + name);
 
+//Exercise 2
 var x = 1125;
 var answer = x % 2;
 console.log(answer);
 
+//Exercise 3
 var a = true;
 var b = false;
 var c = true;
 console.log(a||b||c);
 
+//Exercise 4
+var a = true;
+var b = true;
+var c = true;
+console.log(a&&b&&c);
+
+//Exercise 5
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+console.log(answer1);
+console.log(answer2);
+
+//Exercise 6
+toString(12);
+parseInt("113");
+parseFloat("1.12356");
+var x = parseInt("Hello World");
+console.log(x);
