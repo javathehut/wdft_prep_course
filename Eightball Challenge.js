9fd8ca993a517ed62edf25c9381c4c1b4a493f15
Angelique <javathehut@users.noreply.github.com>
2017-03-23 17:21:13 -0400

    Eightball Switch

--- Eightball Challenge.js
+++ Eightball Challenge.js
@@ -1,5 +1,5 @@
 //magic eightball
-
+/*
 var numberofreplys = Math.floor((Math.random() * 9) + 1);
 function ask(){
     var choice = numberofreplys;
@@ -29,6 +29,47 @@ function ask(){
     } else if(choice == 8){
         reply = "All signs point to a positive outlook!"
         console.log(reply);
+    }
+
+    console.log(choice);
+}
+
+ask();
+*/
+
+// Magic Eightball Using Switch
+
+var numberofreplys = Math.floor((Math.random() * 9) + 1);
+function ask(choice){
+    var choice = numberofreplys;
+    
+    switch (choice) {
+        case 1:
+        console.log("Not gonna happen. Just stay away.");
+        break;
+        case 2:
+        console.log("Forecast does not look good, honey.");
+        break;
+        case 3:
+        console.log("Highly unlikely.");
+        break;
+        case 4:
+        console.log("Probably not but try if you must.");
+        break;
+        case 5:
+        console.log("Chances are 50\-50");
+        break;
+        case 6:
+        console.log("It might work out.");
+        break;
+        case 7:
+        console.log("Hey give it a shot, it looks good!");
+        break;
+        case 8:
+        console.log("All signs point to a positive outlook!");
+        break;
+        default:
+        console.log("Girl, I dunno.")        
     }
 
     console.log(choice);
