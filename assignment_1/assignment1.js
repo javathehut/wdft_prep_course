690ffb09c67a3aa45d2278b9b636d27d60b6c575
Angelique <javathehut@users.noreply.github.com>
2017-03-23 21:46:44 -0400

    Commit Asst1 and Asst2

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -10,8 +10,9 @@ Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
-for(var i=0;i<10;i++){
+for(var i=0;i<11;i++){
     //use the array push() method here.
+    var pushed = numbers.push(i);
 }
 
 
@@ -21,7 +22,7 @@ Test that you used the array push method correctly by printing the sum of the va
 The outputted value should be 9.
 */
 
-
+console.log(numbers[3 + 6]);
 
 
 /*
@@ -29,25 +30,26 @@ Exercise #4
 Use the console.log() function and the array .pop() to add all the values in the array.
 */
 
-var sum = 0;
-/* remove this line to uncomment the code block
+/*var sum = 0;
 while(numbers.length != 0){
     //write pop function here
+
 }
-remove this line to uncomment the code block */
-console.log(sum);
+
+console.log(sum);*/
 
 
 /*
 Exercise #5
 Create an empty object and assign it the variable car below.
 */
-var car;
+var car = {};
 
 /*
-Exercise #5
-Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputing property make to the console. You should see the value "black" printed out.
+Exercise #6
+Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputing property EDIT: colour to the console. You should see the value "black" printed out.
 */
 
+car.colour = "black";
 
-
+console.log(car.colour);
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,7 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
+for(var i = 1; i < 101; i++){
+    console.log(i);
+}
 
 /*
 Exercise #2
@@ -17,3 +19,9 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+var hashTag = "";
+
+for (var i = 0; i < 7; i++) {
+    hashTag = hashTag + "#";
+    console.log(hashTag);
+}
